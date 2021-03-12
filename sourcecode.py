72a13f8e31906738a6e253867d400a9e7c0df405
Riddhima23 <58878761+Riddhima23@users.noreply.github.com>
2021-03-12 21:07:26 +0530

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -1,1 +1,14 @@
+import smtp
+import smtplib
+import speech_recognition as sr
+import pyttsx3
+from email.message import EmailMessage
+import json
 
+listener = sr.Recognizer()
+bot=pyttsx3.init()
+bot. setProperty("rate", 155)
+
+def talk(text):
+    bot.say(text)
+    bot.runAndWait()
