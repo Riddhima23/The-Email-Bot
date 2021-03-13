58c77157cdcbda5925fa16fe62d80654c3b3c3dc
Riddhima23 <58878761+Riddhima23@users.noreply.github.com>
2021-03-14 01:22:54 +0530

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -1,4 +1,3 @@
-#importing modules
 import os
 import json
 import pyttsx3
@@ -12,110 +11,121 @@ import tkinter as tk
 from tkinter import WORD
 from tkinter import messagebox
 
-#sending
+
+# sending
 def send():
     queue.put(e.get())
-    e.delete(0,tk.END)
+    e.delete(0, tk.END)
 
-#speaking
+
+# speaking
 def speak(text):
     bot.say(text)
     bot.runAndWait()
+
 
 # taking input speech command
 def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        queue.put("Kurama:- Listening...\n")
+        queue.put("Kurama:- Listening...\n\n")
         r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
-        queue.put("Kurama:- Recognizing...\n")
+        queue.put("Kurama:- Recognizing...\n\n")
         query = r.recognize_google(audio, language='en-in')
         print(f"You-->{query}\n")
 
     except Exception as e:
-        queue.put("Kurama:- Sorry couldn\'t recognise that !\n")
+        queue.put("Kurama:- Sorry couldn\'t recognise that !\n\n")
         speak("Sorry couldn\'t recognise that !")
-        query="\quit"
+        query = "\quit"
 
     return query
 
-#sending email
-def send_email(sender,pwd,receiver, subject, body):
-    server= smtplib.SMTP('smtp.gmail.com', 587)
+
+# sending email
+def send_email(sender, pwd, receiver, subject, body):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(sender,pwd)
-    email=EmailMessage()
-    email['From']=sender
-    email['To']=receiver
-    email['Subject']=subject
+    server.login(sender, pwd)
+    email = EmailMessage()
+    email['From'] = sender
+    email['To'] = receiver
+    email['Subject'] = subject
     email.set_content(body)
     server.send_message(email)
     server.close()
 
-#data.json(dictionary to json and vice a versa)
+
+# data.json(dictionary to json and vice a versa)
 elist = {}
 with open('data.json') as file:
     elist = json.load(file)
-    
-#main loop    
+
+
+# main loop
 def my_loop(queue):
-    queue.put("Hey pal! This is Kurama , your personal mail assistant, in short  the email bot! I will help you in sending emails with minimum effort !\n \n")
-    speak("Hey pal! This is Kurama , your personal mail assistant, in short  the email bot! I will help you in sending emails with minimum effort ! ")
+    queue.put(
+        "Hey Pal! This is Kurama , your personal Mail Assistant, in short  the Email Bot! I will help you in sending emails with minimum effort !\n \n")
+    speak(
+        "Hey Pal! This is Kurama , your personal Mail Assistant, in short  the Email Bot! I will help you in sending emails with minimum effort ! ")
     while True:
         queue.put("Kurama:- Please enter your email and password:\n")
         speak("Please enter your email: and password:")
-        email=queue.get()
-        queue.put("Sender: ", email)
-        password=queue.get()
-        queue.put("\nKurama:- To whom do you want to send the mail?\n")
+        email = queue.get()
+        password = queue.get()
+        queue.put("Kurama:- To whom do you want to send the mail?\n\n")
         speak("To whom do you want to send the mail?")
-        reply=queue.get()
-        queue.put("Kurama:- Is the email address of the person you want to send email ,present in our mail list ?\n")
-        speak("Is the email of the person you want to send email to present in our mail list ?")
-        queue.put("Email List\n\n")
+        reply = queue.get()
+        queue.put("Kurama:- Is the email address of the person you want to send email to present in our mail list ?\n\n")
+        speak("Is the email address of the person you want to send email to present in our mail list ?")
+        queue.put("Email List:\n\n")
         queue.put(elist)
         queue.put("\n\n")
-        ans=takecommand().lower()
-        if ans=="\quit":
+        ans = takecommand().lower()
+        if ans == "\quit":
             queue.put("\quit")
             break
-        elif ans=="no":
-            queue.put("Kurama:- Enter the email of the recipient:\n")
+        elif ans == "no":
+            queue.put("Kurama:- Enter the email of the recipient:\n\n")
             speak("Enter the email of the recipient:")
-            name=queue.get()
-            elist[reply] =name
+            name = queue.get()
+            elist[reply] = name
             t = len(elist)
             with open('data.json', 'a') as outfile:
                 json.dump(elist, outfile, indent=4)
-        queue.put("Kurama:- What will be the subject of the mail ?\n")
+        queue.put("Kurama:- What will be the subject of the mail ?\n\n")
         speak("What will be the subject of the mail?")
-        sub=takecommand().lower()
-        if sub=="\quit":
+        sub = takecommand().lower()
+        if sub == "\quit":
             queue.put("\quit")
             break
-        queue.put("You:- "+sub+"\n")
-        queue.put("Kurama:- What will be the content of your mail ?\n")
+        queue.put("You:- " + sub + "\n\n")
+        queue.put("Kurama:- What will be the content of your mail ?\n\n")
         speak("What will be the content of your mail?")
-        content=takecommand().lower()
-        if content=="\quit":
+        content = takecommand().lower()
+        if content == "\quit":
             queue.put("\quit")
             break
-        queue.put("You:- "+content+"\n")
-        send_email(email,password,elist[reply],sub,content)
-        speak("Congratulations ! You were able to send your first mail ! Do you want to send more emails or you want to stop ? ")
-        queue.put("Kurama:- Congratulations! You were able to send your first mail! Do you want to send more emails or you want to stop?\n")
-        ans=takecommand().lower()
-        if ans=="\quit":
+        queue.put("You:- " + content + "\n\n")
+        send_email(email, password, elist[reply], sub, content)
+        speak(
+            "Congratulations ! You were able to send your first mail ! Do you want to send more emails or you want to stop ? ")
+        queue.put(
+            "Kurama:- Congratulations! You were able to send your first mail! Do you want to send more emails or you want to stop?\n\n")
+        ans = takecommand().lower()
+        if ans == "\quit":
             queue.put("\quit")
             break
-        elif ans=="stop":
-            queue.put("Kurama-: Sayonara from Kurama ! I hope to see you again ! Till then have a good day! \n")
+        elif ans == "stop":
+            queue.put("Kurama-: Sayonara from Kurama ! I hope to see you again ! Till then have a good day! \n\n")
             speak("Sayonara from Kurama ! I hope to see you again ! Till then have a good day ! ")
             queue.put("\quit")
             break
+
+
 def update_text():
     if not queue.empty():
         text = queue.get()
@@ -127,20 +137,21 @@ def update_text():
 
     root.after(200, update_text)
 
-#driving function
-if __name__ == "__main__":
 
+# driving function
+if __name__ == "__main__":
     bot = pyttsx3.init('sapi5')
     bot.setProperty("rate", 155)
     voices = bot.getProperty('voices')
     bot.setProperty('voice', voices[0].id)
     root = tk.Tk()
-    t = tk.Text(root, height=20, width=100,wrap=WORD)
-    font_tuple=("Teletype", 10)
-    e = tk.Entry(root, width=80,fg="white",font=font_tuple,bg="#293552")
+    t = tk.Text(root, height=20, width=100, wrap=WORD)
+    font_tuple = ("Teletype", 15)
+    e = tk.Entry(root, width=80, fg="white", font=font_tuple, bg="#293552")
     e.grid(row=1, column=0)
-    t.config(background="#07122b",font=font_tuple,fg="white")
-    send = tk.Button(root, text="Send", command=send,bg="blue violet",font=font_tuple,fg="floral white",activebackground="darkgreen",activeforeground="lightcyan").grid(row=1, column=1)
+    t.config(background="#07122b", font=font_tuple, fg="white")
+    send = tk.Button(root, text="Send", command=send, bg="blue violet", font=font_tuple, fg="floral white",
+                     activebackground="darkgreen", activeforeground="lightcyan").grid(row=1, column=1)
     t.grid(row=0, column=0, columnspan=2)
     root.title("KURAMA")
     queue = queue.Queue()
@@ -153,4 +164,3 @@ if __name__ == "__main__":
     root.mainloop()
 
     task.join()
-
